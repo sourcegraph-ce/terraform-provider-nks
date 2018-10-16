7390f88a14e64094b143c2b223b5390b1c5e6a73
jasminSPC <jasmin@stackpointcloud.com>
2018-10-16 18:46:49 +0200

    nks_keysets enhanced with additional filters

--- nks/datasource_nks_keysets.go
+++ nks/datasource_nks_keysets.go
@@ -2,8 +2,10 @@ package stackpoint
 
 import (
 	"fmt"
-	"log"
 	"strconv"
+	"strings"
+
+	"github.com/StackPointCloud/stackpoint-sdk-go/stackpointio"
 
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -16,37 +18,24 @@ func dataSourceNKSKeysets() *schema.Resource {
 				Type:     schema.TypeInt,
 				Optional: true,
 			},
-			"aws_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
+			"category": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: func(v interface{}, k string) (ws []string, errors []error) {
+					filter := v.(string)
+					if filter != "provider" && filter != "user" {
+						errors = append(errors, fmt.Errorf("category can be either 'provider' or 'user'"))
+					}
+					return
+				},
 			},
-			"azure_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
+			"entity": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
-			"do_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"gce_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"gke_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"oneandone_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"packet_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"user_ssh_keyset": {
-				Type:     schema.TypeInt,
-				Computed: true,
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 		},
 	}
@@ -54,37 +43,77 @@ func dataSourceNKSKeysets() *schema.Resource {
 
 func dataSourceNKSKeysetsRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
+	var name string
+	var entity string
+	category := d.Get("category").(string)
+	if category == "provider" {
+		if temp, ok := d.GetOk("entity"); ok {
+			entity = temp.(string)
+		} else {
+			return fmt.Errorf("if category is set to 'provider' entity must be set")
+		}
+	}
+
+	if tmp, ok := d.GetOk("name"); ok {
+		name = tmp.(string)
+	}
 
 	// Fetch userprofile based on API token
-	up, err := config.Client.GetUserProfile()
+	userProfile, err := config.Client.GetUserProfile()
 	if err != nil {
-		log.Printf("[DEBUG] Keysets GetUserProfile failed: %s\n", err)
 		return err
 	}
-	if up == nil {
-		return fmt.Errorf("Could not fetch user profile, cannot proceed with keyset import from NKSCloud\n")
-	}
 
-	// Use supplied org ID or fetch org ID from userprofile
-	var orgID int
-	if _, ok := d.GetOk("org_id"); !ok {
-		orgID, err = config.Client.GetUserProfileDefaultOrg(&up[0])
-		if err != nil {
-			log.Printf("[DEBUG] Keysets GetUserProfileDefaultOrg failed: %s\n", err)
-			return err
+	var userKeys []stackpointio.Keyset
+	var providerKeys []stackpointio.Keyset
+
+	for _, c := range userProfile[0].Keysets {
+		if category == "provider" {
+			providerKeys = append(providerKeys, c)
+		} else if category == "user" {
+			userKeys = append(userKeys, c)
 		}
-		d.Set("org_id", orgID)
-	} else {
-		orgID = d.Get("org_id").(int)
 	}
 
-	// Loop through keysets loaded into userprofile, store them into variables
-	for _, ks := range []string{"aws", "azure", "do", "gce", "gke", "oneandone", "packet", "user_ssh"} {
-		ksid, _ := config.Client.GetUserProfileKeysetID(&up[0], ks)
-		d.Set(ks+"_keyset", ksid)
+	if len(providerKeys) > 0 {
+		var subKeys []stackpointio.Keyset
+		for _, p := range providerKeys {
+			if entity == p.Entity {
+				subKeys = append(subKeys, p)
+			}
+		}
+		if name != "" {
+			var newKeys []stackpointio.Keyset
+
+			for _, p := range subKeys {
+
+				if strings.Contains(strings.ToLower(p.Name), strings.ToLower(name)) {
+					newKeys = append(newKeys, p)
+				}
+			}
+			subKeys = newKeys
+		}
+		if len(subKeys) > 1 {
+			return fmt.Errorf("there is more than keyset in category '%s' and entity '%s' refine the search with 'name' parameter ", category, entity)
+		}
+		d.SetId(strconv.Itoa(subKeys[0].ID))
+		return nil
 	}
+	if len(userKeys) > 0 {
+		var subKeys []stackpointio.Keyset
+		if name != "" {
+			for _, u := range userKeys {
+				if strings.Contains(strings.ToLower(u.Name), strings.ToLower(name)) {
+					subKeys = append(subKeys, u)
+				}
+			}
+		}
 
-	d.SetId(strconv.Itoa(orgID))
-
+		if len(subKeys) > 1 {
+			return fmt.Errorf("there is more than keyset in category '%s' refine the search with 'name' parameter ", category)
+		}
+		d.SetId(strconv.Itoa(subKeys[0].ID))
+		return nil
+	}
 	return nil
 }
